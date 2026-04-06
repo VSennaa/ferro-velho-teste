95b9a2451941877b83c28a052d39964833198572
VSenna <viniciosrodrigo.martins@gmail.com>
2026-04-05 22:51:08 -0300

    Ajustado tipo para categoria

--- ferrovelho_app/urls.py
+++ ferrovelho_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 1. A Raiz do site (O Dashboard principal)
+    path('', views.dashboard, name='dashboard'), 
+    
+    # 2. As rotas que você já tinha
     path('materials/', views.material_list_create, name='material_list_create'),
     path('materials/<int:pk>/edit/', views.material_edit, name='material_edit'),
     path('pdv/', views.operacao_pdv, name='operacao_pdv'),
